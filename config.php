43bf9783d34cd7c99c8ca6f38fea86c7ece5c569
Atles <taleslimaf@gmail.com>
2013-07-26 08:02:17 -0300

    Mudança: Conexão do Banco

--- config.php
+++ config.php
@@ -1,6 +1,19 @@
-<?php
-$conexao = mysql_connect("mysql02.redehost.com.br","lucas.ufjf","99l9m2g1");
-//$conexao = mysql_connect("localhost","root","");
-mysql_select_db("politicos_brasileiros", $conexao);
-mysql_set_charset("utf8");
+<?php include 'properties.php';
+	
+	$dados = new Constant;
+	$host = getProperty($dados->DB_HOST);
+	$user = getProperty($dados->DB_USER);
+	$pass = getProperty($dados->DB_PASS);
+	$pltc = getProperty($dados->DB_PLTC);
+	
+	$conexao = mysql_connect($host,$user,$pass);
+	if(!$conexao){
+    		die('Não foi possível conectar: ' . mysql_error());
+	}
+	echo 'Conexão bem sucedida';
+
+	//$conexao = mysql_connect("localhost","root","");
+	mysql_select_db($pltc, $conexao);
+	mysql_set_charset("utf8");
+
 ?>
